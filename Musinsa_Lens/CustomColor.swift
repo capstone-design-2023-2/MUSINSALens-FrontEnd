0074ba19cbcf3b7c3c0e919af162f8eb2e824303
yerin <81500522+YerinDoi@users.noreply.github.com>
2023-11-24 01:26:48 +0900

    검색화면 완료

---
+++ Musinsa_Lens/CustomColor.swift
@@ -0,0 +1,18 @@
+//
+//  CustomColor.swift
+//  Musinsa_Lens
+//
+//  Created by 박예린 on 2023/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct CustomColor {
+    static let beige = UIColor(red: 255 / 255, green: 231 / 255, blue: 204 / 255, alpha: 1)
+    static let sage = UIColor(red: 225 / 255, green: 238 / 255, blue: 221 / 255, alpha: 1)
+    static let deepGreen = UIColor(red: 24 / 255, green: 58 / 255, blue: 29 / 255, alpha: 1)
+    static let gray = UIColor(red: 163/255, green: 163/255, blue: 163/255, alpha: 1.0)
+    static let lightgray = UIColor(red: 242/255, green: 241/255, blue: 241/255, alpha: 1.0)
+    
+}
