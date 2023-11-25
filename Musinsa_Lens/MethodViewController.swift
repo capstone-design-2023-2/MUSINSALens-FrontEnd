6410ca2f7e6be1245a37bd60937bb0964a482ccf
yerin <81500522+YerinDoi@users.noreply.github.com>
2023-11-25 18:01:49 +0900

    Method View 시작

--- Musinsa_Lens/MethodViewController.swift
+++ Musinsa_Lens/MethodViewController.swift
@@ -12,14 +12,28 @@ class MethodViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
     }
-    
 
-    /*
+    // MARK: - Private Methods
+
+    private func setupNavigationBar() {
+        // 네비게이션 바 색상 설정
+        navigationController?.navigationBar.barTintColor = .black
+        
+        // 네비게이션 바 타이틀 설정
+        navigationItem.title = "무신사렌즈"
+        
+        // 네비게이션 바 타이틀 색상 설정
+        let titleTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.black
+        ]
+        navigationController?.navigationBar.titleTextAttributes = titleTextAttributes
+    }
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
