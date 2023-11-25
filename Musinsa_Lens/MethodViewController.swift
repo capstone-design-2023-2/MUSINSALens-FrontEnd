8b5a374501d0f9873b41aa5ce1644b299bf1de01
yerin <81500522+YerinDoi@users.noreply.github.com>
2023-11-25 18:01:49 +0900

    Method View 시작

--- Musinsa_Lens/MethodViewController.swift
+++ Musinsa_Lens/MethodViewController.swift
@@ -11,15 +11,23 @@ class MethodViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupNavigationBar()
     }
-    
 
-    /*
+    // MARK: - Private Methods
+
+    private func setupNavigationBar() {
+        
+        let navigationController = UINavigationController(rootViewController: SearchViewController())
+        
+        // 네비게이션 바 타이틀 설정
+        navigationItem.title = "무신사 렌즈"
+    }
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
