913e824fb5557f7900c5b495987a608764bcfd59
yerin <81500522+YerinDoi@users.noreply.github.com>
2023-11-26 20:55:41 +0900

    추천 화면 셀 완료

--- Musinsa_Lens/LoginViewController.swift
+++ Musinsa_Lens/LoginViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import KakaoSDKUser
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // 카카오톡으로 로그인
+    @IBAction func loginWithKakaoTalk(){
+        // 카카오톡 실행 가능 여부 확인
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+                    
+                    //do something
+                    _ = oauthToken
+                }
+            }
+        }
+        // 카카오 계정으로 로그인
+        else{UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+                
+                //do something
+                _ = oauthToken
+            }
+        }
+        }
+    }
 }
-
--- Musinsa_Lens/RecommendViewController.swift
+++ Musinsa_Lens/RecommendViewController.swift
@@ -1,31 +1,25 @@
 //
-//  MethodViewController.swift
+//  RecommendViewController.swift
 //  Musinsa_Lens
 //
-//  Created by 박예린 on 2023/11/20.
+//  Created by 박예린 on 2023/11/26.
 //
 
 import UIKit
 
-class MethodViewController: UIViewController {
+class RecommendViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupNavigationBar()
+
+        // Do any additional setup after loading the view.
     }
-
-    // MARK: - Private Methods
-
-    private func setupNavigationBar() {
-        
-        // 네비게이션 바 타이틀 설정
-        navigationItem.title = "무신사 렌즈"
-    }
-
-    // MARK: - Navigation
+    
 
     /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
