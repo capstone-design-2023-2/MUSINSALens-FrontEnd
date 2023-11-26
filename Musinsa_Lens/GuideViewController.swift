0cba5b4c4bf63b7c15c5e520bbe2876dd180ec1d
yerin <81500522+YerinDoi@users.noreply.github.com>
2023-11-26 22:27:46 +0900

    추천 화면 셀 완료

---
+++ Musinsa_Lens/GuideViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GuideViewController.swift
+//  Musinsa_Lens
+//
+//  Created by 박예린 on 2023/11/26.
+//
+
+import UIKit
+
+class GuideViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func confirmButtonTapped(_sender: UIButton) {
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
